a06e2f9ce6c1b9d49314a6dd525bb6c169d9507c
sanya-kenneth <sanyakenneth@gmail.com>
2019-03-12 10:26:52 +0300

    Add User model for the api

---
+++ api/auth/models.py
@@ -0,0 +1,16 @@
+from api.database.db import db
+
+
+class User(db.Model):
+    
+    user_id = db.Column('user_id',db.Integer, primary_key=True)
+    firstname = db.Column('first_name',db.String(80), unique=True, nullable=False)
+    lastname = db.Column('last_name',db.String(80), unique=True, nullable=False)
+    email = db.Column('user_email',db.String(120), unique=True, nullable=False)
+    phone_number = db.Column('phone_number',db.Integer, unique=True, nullable=False)
+    password = db.Column('user_password',db.Text, unique=True, nullable=False)
+    account_type = db.Column('account_type',db.String(20), unique=True, nullable=False)
+
+    
+    def __repr__(self):
+        return '<User %r>' % self.username
