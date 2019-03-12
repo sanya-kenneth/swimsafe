1cc6300d3c0462b81ff19a379ad9b2f8f31fffc8
sanya-kenneth <sanyakenneth@gmail.com>
2019-03-12 14:23:31 +0300

    add login an signup views

--- api/auth/views.py
+++ api/auth/views.py
@@ -1,2 +1,18 @@
 from flask import jsonify
 from api.auth import auth_bp
+# from api.auth.models import User
+from api.auth.controllers import UserController
+from api.database.db import db
+
+
+user = UserController()
+
+
+@auth_bp.route('/users', methods=['POST'])
+def register_user():
+    return user.signup_user()
+
+
+@auth_bp.route('/users/login', methods=['POST'])
+def signin_user():
+    return user.login_user()
