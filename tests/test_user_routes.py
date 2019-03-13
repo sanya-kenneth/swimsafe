0488c896f74295e879785b58201e64dd57b532c9
sanya-kenneth <sanyakenneth@gmail.com>
2019-03-13 16:41:37 +0300

    Add tests for user routes

---
+++ tests/test_user_routes.py
@@ -0,0 +1,158 @@
+from api.auth import views
+from tests.base import BaseTest
+import json
+
+
+class UserTests(BaseTest):
+    def test_returns_error_if_firstname_is_missing(self):
+        data = {
+                "lastname":"10",
+                "email":"ken@you.com",
+                "phonenumber": 706578719,
+                "password": "dfghjklkjhg3434",
+                "confirmpassword":"dfghjklkjhg3434"
+                
+            }
+        res = self.app.post('/api/v1/users', content_type="application/json",
+        data=json.dumps(data))
+        response_data = json.loads(res.data.decode())
+        self.assertEqual(res.status_code,400)
+        self.assertEqual(response_data['error'], "firstname is missing")
+
+
+    def test_returns_error_if_lastname_is_missing(self):
+        data = {
+                "firstname": "ken",
+                "email":"ken@you.com",
+                "phonenumber": 706578719,
+                "password": "dfghjklkjhg3434",
+                "confirmpassword":"dfghjklkjhg3434"
+                
+            }
+        res = self.app.post('/api/v1/users', content_type="application/json",
+        data=json.dumps(data))
+        response_data = json.loads(res.data.decode())
+        self.assertEqual(res.status_code,400)
+        self.assertEqual(response_data['error'], "lastname is missing")
+
+
+    def test_returns_error_if_email_is_missing(self):
+        data = {
+                "firstname": "ken",
+                "lastname": "ken",
+                "phonenumber": 706578719,
+                "password": "dfghjklkjhg3434",
+                "confirmpassword":"dfghjklkjhg3434"
+                
+            }
+        res = self.app.post('/api/v1/users', content_type="application/json",
+        data=json.dumps(data))
+        response_data = json.loads(res.data.decode())
+        self.assertEqual(res.status_code,400)
+        self.assertEqual(response_data['error'], "email is missing")     
+
+
+    def test_returns_error_if_phonenumber_is_missing(self):
+        data = {
+                "firstname": "ken",
+                "lastname": "ken",
+                "email":"ken@you.com",
+                "password": "dfghjklkjhg3434",
+                "confirmpassword":"dfghjklkjhg3434"
+                
+            }
+        res = self.app.post('/api/v1/users', content_type="application/json",
+        data=json.dumps(data))
+        response_data = json.loads(res.data.decode())
+        self.assertEqual(res.status_code,400)
+        self.assertEqual(response_data['error'], "phonenumber is missing") 
+
+
+    def test_returns_error_if_password_is_missing(self):
+        data = {
+                "firstname": "ken",
+                "lastname": "ken",
+                "phonenumber": 706578719,
+                "email":"ken@you.com",
+                "confirmpassword":"dfghjklkjhg3434"
+                
+            }
+        res = self.app.post('/api/v1/users', content_type="application/json",
+        data=json.dumps(data))
+        response_data = json.loads(res.data.decode())
+        self.assertEqual(res.status_code,400)
+        self.assertEqual(response_data['error'], "password is required")  
+
+
+    def test_returns_error_if_passwords_dont_match(self):
+        data = {
+                "firstname": "ken",
+                "lastname": "ken",
+                "phonenumber": 706578719,
+                "email":"ken@you.com",
+                "password":"dfghjklkjhg3434",
+                "confirmpassword":"dfghjklkjhg3434dfghjkjgfdfgh12332"
+                
+            }
+        res = self.app.post('/api/v1/users', content_type="application/json",
+        data=json.dumps(data))
+        response_data = json.loads(res.data.decode())
+        self.assertEqual(res.status_code,400)
+        self.assertEqual(response_data['error'], "Passwords don't match") 
+
+
+    def test_user_can_signup(self):
+        data = {
+                "firstname": "ken",
+                "lastname":"10",
+                "email":"ken@you.com",
+                "phonenumber": 706578719,
+                "password": "dfghjklkjhg3434",
+                "confirmpassword":"dfghjklkjhg3434"
+                }
+        res = self.app.post('/api/v1/users', content_type="application/json",
+        data=json.dumps(data))
+        response_data = json.loads(res.data.decode())
+        self.assertEqual(res.status_code,201)
+        self.assertEqual(response_data['message'], "Your account was successfuly created")
+
+
+    def test_user_can_login(self):
+        data = {
+                "firstname": "pen",
+                "lastname":"10",
+                "email":"pen@you.com",
+                "phonenumber": 706578718,
+                "password": "dfghjklkjhg343411111",
+                "confirmpassword":"dfghjklkjhg343411111"
+                
+            }
+        user_data_login = {
+            "email": "pen@you.com",
+            "password": "dfghjklkjhg343411111"
+        }
+        self.app.post('/api/v1/users', content_type="application/json",
+        data=json.dumps(data))
+        res = self.app.post('/api/v1/users/login', content_type="application/json",
+                            data=json.dumps(user_data_login))
+        response_data = json.loads(res.data.decode())
+        self.assertEqual(res.status_code,200)
+        self.assertEqual(response_data['message'], "You are now loggedin")
+
+
+    def test_gets_data_for_one_user(self):
+        res = self.app.get('/api/v1/user', content_type="application/json",
+        headers=self.user_header())
+        response_data = json.loads(res.data.decode())
+        self.assertEqual(res.status_code,200)
+        self.assertEqual(response_data['status'], 200)
+        self.assertIn("ten@you.com", response_data['data']['email'])
+
+
+    def test_gets_data_for_all_users(self):
+        res = self.app.get('/api/v1/users', content_type="application/json",
+        headers=self.admin_header())
+        response_data = json.loads(res.data.decode())
+        self.assertEqual(res.status_code,200)
+        self.assertEqual(response_data['status'], 200)
+        self.assertIsInstance(response_data['data'], list)
